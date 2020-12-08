205d7ebafb3580e4fd1072dce42cc50ebe7e664f
yutatanomoki <yuta3071@gmail.com>
2020-12-08 13:11:01 +0000

    [Finish]4-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'd2f20e9e817318a1b1e59776f74c4f78e2686e88536c42073c35150fc124b9c97393eed7799fb82c1d30c6a3fc17500d58ce40025ae245615a36a5a3d1df4deb'
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root 'post_images#index'
   devise_for :users
-  root to: 'homes#top'
+  #root to: 'homes#top'
+  resources :post_images, only: [:new, :create, :index, :show, :destroy]
 end
