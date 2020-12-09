5a5d99de72799a99e9f1a304633f0f84a6dd7fc0
yutatanomoki <yuta3071@gmail.com>
2020-12-09 09:35:00 +0000

    [Finish]4-5

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
-  get 'users/show'
-  root 'post_images#index'
+  #get 'users/show'
   devise_for :users
-  #root to: 'homes#top'
+  root 'post_images#index'
+  
   resources :post_images, only: [:new, :create, :index, :show, :destroy] do
     resource :favorites, only: [:create, :destroy]
     resources :post_comments, only: [:create, :destroy]
   end
-  
+  resources :users, only: [:show, :edit, :update]
 end
+
